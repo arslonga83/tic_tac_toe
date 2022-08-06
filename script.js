0460e808af614f4f9f10b38cbf3f05ce092e79a8
arslonga83 <jlwood83@gmail.com>
2022-08-06 15:53:58 -0700

    make symbols trade turns

--- script.js
+++ script.js
@@ -20,18 +20,20 @@ const playerFactory = (name, symbol) => {
 
     const setName = () => name = prompt('name: ');
 
-    function makeMove() {
-        for (i = 0; i < 9; i++) {
-            let index = i;
-            document.getElementById('box' + i).addEventListener('click', () => {
-                GameBoard.spaces.splice(index, 1, this.symbol);
-                GameBoard.setupBoard();  
-            })
-        }
-    }
-    return {name, symbol, setName, makeMove};
+    //function makeMove() {
+    //    for (i = 0; i < 9; i++) {
+    //        let index = i;
+    //        document.getElementById('box' + i).addEventListener('click', () => {
+    //            GameBoard.spaces.splice(index, 1, this.symbol);
+    //            GameBoard.setupBoard(); 
+    //            move += 1;
+    //            console.log(move);
+                
+    //        })
+    //    }
+   // }
+    return {name, symbol, setName};
 };
-
 
 
 
@@ -40,26 +42,33 @@ const playerFactory = (name, symbol) => {
 const playGame = (() => {
     const player1 = playerFactory('player1', 'X');
     const player2 = playerFactory('player2', 'O');
-  
+    let move = 0;
 
-    takeTurns();
+    for (i = 0; i < 9; i++) {
+        let index = i;
+        
+        document.getElementById('box' + i).addEventListener('click', () => {
+            if (move % 2 === 0) {
+                console.log(move)
+                GameBoard.spaces.splice(index, 1, player1.symbol); 
+            }
+            else {
+                console.log(move)
+                GameBoard.spaces.splice(index, 1, player2.symbol); 
+            }
 
-  function takeTurns() {
-    let turn = 'X';
-    if (turn === 'X') {
-        player1.makeMove();
-        turn = 'O';
-        console.log(turn);
-    }
-    else if (turn === 'O') {
-        player2.makeMove();
-        turn = 'X';
-        console.log(turn);
-    }
-  }
- 
+            
+        
+        
+            
+                    GameBoard.setupBoard(); 
+                    move += 1;
+                    console.log(move);
+        
 
-})()
+                })
+
+}})()
 
 
-
+   
