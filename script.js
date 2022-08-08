bcca3819131d9c3e6a343c1c8a1bfd24514c84fd
arslonga83 <jlwood83@gmail.com>
2022-08-07 20:57:53 -0700

    add html status updates. prompt for names

--- script.js
+++ script.js
@@ -23,16 +23,14 @@ const GameBoard = (() => {
 
     function checkDraw() {
        if (spaces.includes("") === false) {
-        console.log('draw');
-        console.log('play again?');
+        display.status.innerHTML = 'It\'s a draw. Play again?'
         document.getElementById('reset').disabled = false;
         document.getElementById('grid-container').outerHTML = document.getElementById('grid-container').outerHTML;
        }
     }
 
     function endRound(winner) {
-        console.log(winner + ' is the winner!');
-        console.log('play again?');
+        display.status.innerHTML = winner + ' is the winner! Click reset button to play again.';
         document.getElementById('reset').disabled = false;
         document.getElementById('grid-container').outerHTML = document.getElementById('grid-container').outerHTML;
     }
@@ -65,15 +63,25 @@ const playerFactory = (name, symbol) => {
     return {name, symbol, setName};
 };
 
+const display = (() => {
+    const status = document.getElementById('status');
+    return {
+        status
+    }
+})()
+
 //playgame module
 const playGame = (() => {
     const player1 = playerFactory('player1', 'X');
     const player2 = playerFactory('player2', 'O');
+    player1.name = prompt('name:');
+    player2.name = prompt('name:');
     let move = 0;
     playRound();
 
     function playRound() {
-    for (i = 0; i < 9; i++) {
+        display.status.innerHTML = player1.name + ' goes first...';
+        for (i = 0; i < 9; i++) {
         let index = i;
         let square = document.getElementById('box' + i);
         
@@ -84,6 +92,8 @@ const playGame = (() => {
             if (move % 2 === 0) {
                 GameBoard.spaces.splice(index, 1, player1.symbol);
                 GameBoard.setupBoard();
+                
+                display.status.innerHTML = player2.name + '\'s turn';
                 GameBoard.checkDraw();
                 if (GameBoard.checkWin(player1.symbol) === true) {
                     GameBoard.endRound(player1.name);
@@ -92,6 +102,8 @@ const playGame = (() => {
             else {
                 GameBoard.spaces.splice(index, 1, player2.symbol); 
                 GameBoard.setupBoard();
+                
+                display.status.innerHTML = player1.name + '\'s turn';
                 GameBoard.checkDraw();
                 if (GameBoard.checkWin(player2.symbol) === true) {
                     GameBoard.endRound(player2.name);
